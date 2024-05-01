dfc62bf22bef06fb48dfa5e9c5476f21d89b0e51
Chung Xa <wall85vn@yahoo.com>
2024-05-01 20:22:35 +0700

    Update WebViewJNI.java

--- webview/src/java/com/defold/webview/WebViewJNI.java
+++ webview/src/java/com/defold/webview/WebViewJNI.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.annotation.TargetApi;
 import android.net.Uri;
 import android.os.Build;
 import android.view.Gravity;
